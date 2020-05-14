99e1a19dbdb65b31647588ce0300ee92abfc7174
MAVArce <marquinhos_213@hotmail.com>
2020-05-14 17:04:36 -0300

    Box responding to number selected

--- TLine/mainwindow.cpp
+++ TLine/mainwindow.cpp
@@ -1,14 +1,49 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <iostream>
+
+#define MAX 101
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
     ui(new Ui::MainWindow){
+
     ui->setupUi(this);
+
+    zBox = ui->zBox;
+    tBox = ui->tBox;
+
+    for(int i = 0; i < MAX; i++){
+        zBox->addItem(QString::number(i));
+        tBox->addItem(QString::number(i));
+    }
+
+    QSize iconsSize(46, 46);
+
+    ui->firstV->setIcon(QIcon(":/icons/firstV.png"));
+    ui->secondV->setIcon(QIcon(":/icons/secondV.png"));
+    ui->firstR->setIcon(QIcon(":/icons/firstR.png"));
+    ui->secondR->setIcon(QIcon(":/icons/secondR.png"));
+    ui->thirdR->setIcon(QIcon(":/icons/thirdR.png"));
+
+    ui->firstV->setIconSize(iconsSize);
+    ui->secondV->setIconSize(QSize(120, 180));
+    ui->firstR->setIconSize(iconsSize);
+    ui->secondR->setIconSize(iconsSize);
+    ui->thirdR->setIconSize(iconsSize);
+
+    this->setWindowTitle("Transmission Lines");
+
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
+void MainWindow::on_zBox_currentIndexChanged(int index){
+    std::cout << index << std::endl;
+}
 
+void MainWindow::on_tBox_currentIndexChanged(int index){
+    std::cout << index << std::endl;
+}
--- TLine/mainwindow.h
+++ TLine/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtWidgets>
 
 namespace Ui {
 class MainWindow;
@@ -14,8 +15,20 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+signals:
+    void timeChanged(int newTime);
+    void zPosChanged(int newZPos);
+
+private slots:
+    void on_zBox_currentIndexChanged(int index);
+    void on_tBox_currentIndexChanged(int index);
+
+    void on_firstV_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QComboBox *zBox;
+    QComboBox *tBox;
 };
 
 #endif // MAINWINDOW_H
