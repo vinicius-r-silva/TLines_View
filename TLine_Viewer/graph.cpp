6a9695b947e4ce2de37e91ce74c28ca08371029b
vinicius-r-silva <vinicius.r@usp.br>
2020-05-17 00:48:02 -0300

    created graph class

---
+++ TLine_Viewer/graph.cpp
@@ -0,0 +1,14 @@
+
+#include "graph.h"
+#include "math.h"
+
+#define _Z0 0
+#define _ZF 
+
+float avalia(float x){
+    return 2*cos(0.39*x) + 5*sin(0.5*x) + 0.5*cos(0.1*x) + 10*sin(0.7*x) + 5*sin(1*x) + 5*sin(0.35*x);
+}
+
+Graph::Graph(){
+    
+}
---
+++ TLine_Viewer/graph.h
@@ -0,0 +1,16 @@
+#ifndef GRAPH_CLASS
+#define GRAPH_CLASS
+
+
+class Graph{
+
+private:
+
+public:
+    Graph();
+
+};
+
+
+
+#endif //GRAPH_CLASS
