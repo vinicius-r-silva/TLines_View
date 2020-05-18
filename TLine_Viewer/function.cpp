70961b7bfa0a79ef242d6a25c11c7d0d335ce804
MAVArce <marquinhos_213@hotmail.com>
2020-05-17 21:03:11 -0300

    Add parameters to calculateAllValues

--- TLine_Viewer/function.cpp
+++ TLine_Viewer/function.cpp
@@ -1,8 +1,11 @@
 #include "function.h"
 #include <iostream>
 
-functionData_t calculateAllValues()
-{
+functionData_t calculateAllValues(float dt, int nt, float dz, int nz){
+    double C1 = (-2*dt) / (dt*dz*R + 2*dz*L);
+    double C2 = (2*L - dt*R) / (2*L + dt*R);
+    double C3 = (-2*dt) / (dt*dz*G + 2*dz*C);
+    double C4 = (2*C - dt*G) / (2*C + dt*G);
 
     int t;
     int z;
@@ -10,14 +13,14 @@ functionData_t calculateAllValues()
     double **voltage;
     double **current;
 
-    voltage = new double *[N + 1];
-    current = new double *[N + 1];
+    voltage = new double *[nt + 1];
+    current = new double *[nt + 1];
 
     std::cout << "iniciando alocacao" << std::endl;
-    for (t = 0; t < N + 1; t++)
+    for (t = 0; t < nt + 1; t++)
     {
-        voltage[t] = new double[K + 1];
-        current[t] = new double[K + 1];
+        voltage[t] = new double[nz + 1];
+        current[t] = new double[nz + 1];
 
         if (voltage[t] == nullptr || current[t] == nullptr)
         {
@@ -29,13 +32,13 @@ functionData_t calculateAllValues()
     std::cout << "iniciando calculo" << std::endl;
 
     //inicial values
-    for (t = 0; t < N + 1; t++)
+    for (t = 0; t < nt + 1; t++)
     {
         voltage[t][0] = 2;
         current[t][0] = 2 / (Z0 + Zl + Rs);
     }
 
-    for (z = 1; z < K; z++)
+    for (z = 1; z < nz; z++)
     {
         voltage[0][z + 1] = 0;
         current[0][z + 1] = 0;
@@ -43,12 +46,12 @@ functionData_t calculateAllValues()
 
     std::cout << "parte inicial completa" << std::endl;
 
-    for (t = 1; t < N; t++)
+    for (t = 1; t < nt; t++)
     {
-        for (z = 1; z < K; z++)
+        for (z = 1; z < nz; z++)
             current[t + 1][z] = C1 * (voltage[t][z + 1] - voltage[t][z - 1]) + C2 * current[t - 1][z];
 
-        for (z = 1; z < K; z++)
+        for (z = 1; z < nz; z++)
             voltage[t + 1][z + 1] = C3 * (current[t + 1][z + 1] - current[t + 1][z]) + C4 * current[t][z + 1];
 
         std::cout << "t: " << t << std::endl;
@@ -57,9 +60,9 @@ functionData_t calculateAllValues()
     std::cout << "calculo principal terminado" << std::endl;
 
     //curto circuito
-    for (t = 0; t < N; t++)
+    for (t = 0; t < nt; t++)
     {
-        voltage[t + 1][K] = 0;
+        voltage[t + 1][nz] = 0;
     }
 
     std::cout << "calculo terminado" << std::endl;
--- TLine_Viewer/function.h
+++ TLine_Viewer/function.h
@@ -21,17 +21,8 @@
 #define Zl 0
 //#define Zl 100
 
-const int K = l/DZ;
-const int N = MAX_TIME/DT;
-
 const double C = 7.40741 * powf64x(10, -11);
 const double L = 1.85185 * powf64x(10, -7);
-
-const double C1 = (-2*DT) / (DT*DZ*R + 2*DZ*L);
-const double C2 = (2*L - DT*R) / (2*L + DT*R);
-const double C3 = (-2*DT) / (DT*DZ*G + 2*DZ*C);
-const double C4 = (2*C - DT*G) / (2*C + DT*G);
-
 
 typedef struct functionData_t{
 
@@ -41,4 +32,4 @@ typedef struct functionData_t{
 }functionData_t;
 
 double initialFunction(double t);
-functionData_t calculateAllValues();
+functionData_t calculateAllValues(float dt, int nt, float dz, int nz);
